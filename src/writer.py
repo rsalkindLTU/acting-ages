3ba036ab9adab5e10a4a6624b405ac754730ca29
Rhys Salkind <rsalkind@ltu.edu>
2018-11-08 20:08:00 -0500

    Creating dir, writing to file.

--- src/writer.py
+++ src/writer.py
@@ -1,8 +1,58 @@
+import os as oos
+from pathlib import Path
+
 # This fill will eventually be used for writing all the good information to the disk
-# it will take each actors name and list a things in a .csv for opening with an excel 
+# it will take each actors name and list a things in a .csv for opening with an excel
 # format to create the charts
+save_path = "../out/"
+
+def path_init():
+    # Function to create the folder for the output files
+    # Does nothing if the folder already exists
+    if not Path(save_path).exists():
+        try:
+            oos.mkdir(save_path)
+        except OSError:
+            print("Creation of output file failed")
+            raise
+    else:
+        pass
+
+def writeFormatted(f_stream, args, collumn_offset = 0):
+    for x in range(collumn_offset):
+        f_stream.write(',')
+    for a in args:
+        if ',' in a:
+            f_stream.write('"' + a  + '",')
+        else:
+            f_stream.write(a + ",")
+
+    f_stream.write('\n')
+
+def movie_group(actors, actor_name, f):
+    # This function will take the list of actresses in one movie and
+    # order them with the actor at the top, followed by their respective
+    # ages in the movie.
+    #print(actors)
+    dude = None
+    for a in actors:
+        if a['actor'] == actor_name:
+            dude = a
+        else:
+            pass
+
+    writeFormatted(f, ['Movie:', 'Actor:', 'Actress:', 'Age:'], 2)
+    writeFormatted(f, [dude['movie'], dude['actor'], '', str(dude['age'])], 2)
+
+
+    for a in actors:
+        if a['actor'] == actor_name:
+            continue
+        else:
+            writeFormatted(f, [a['actor'], str(a['age'])], 4)
 
 def write(actors, actor_name):
+    path_init()
     # First order of buisness: Filter the list (again) into two seperate lists
     # One of the thrown out movies
     # The other of all good information
@@ -11,16 +61,46 @@ def write(actors, actor_name):
     good_data = []
     for a in actors:
         if type(a) is str:
-            thrown_data.append(a)
-        elif type(a) is dict:
+            thrown_data.append((a, 'doA'))
+        elif a['gpr_hit'] is False and a['net_hit'] is False:
+            thrown_data.append((a['movie'], 'not_hit'))
+        else:
             filtered_data = {'actor':a['lead'], 'movie':a['movie'], 'age':a['age_at_release'], 'year':a['movie_year_released']}
             good_data.append(filtered_data)
 
     good_data = sorted(good_data, key=lambda k: k['year'])
+    thrown_data = list(set(thrown_data))
 
     print("Good Data: ")
     print(good_data)
     print("Bad data: ")
     print(thrown_data)
-    #raise SyntaxError("The write module is not defined yet")
+
+    # Start writing!
+    act_file_nm = actor_name + ".csv"
+    with open(oos.path.join(save_path, act_file_nm), "a") as f:
+
+        # Loop over bad data first
+        writeFormatted(f, ['\n', '\n', '\n', 'Bad Data', actor_name])
+        writeFormatted(f, ['Movie','Throw Reason'], 2)
+        for t in thrown_data:
+            writeFormatted(f, [t[0], t[1]], 2)
+
+        # Now for the good data!
+        writeFormatted(f, ['\n', '\n', '\n', 'Good Data', actor_name])
+        writeFormatted(f, ['Movie'], 2)
+        single_movie_data = []
+        current_movie = good_data[0]['movie']
+        for g in good_data:
+            #writeFormatted(f, ['Nothing', 'Yet'], 2)
+            if g['movie'] != current_movie:
+                #print("old m: " + current_movie + ", new m: " + g['movie'])
+                #print("current g actor: " + g['actor'])
+                current_movie = g['movie']
+                movie_group(single_movie_data, actor_name, f)
+                single_movie_data = []
+                single_movie_data.append(g)
+            else:
+                single_movie_data.append(g)
+                #print(g)
 
